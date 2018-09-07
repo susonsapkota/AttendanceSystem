0b3554cf80ab56de0ab19b1c52ea33653be275cd
susonsapkota <susonsapkota@gmail.com>
2018-09-07 23:59:00 +0545

    Added Login page

--- footer.php
+++ footer.php
@@ -3,24 +3,8 @@
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script><!-- Footer -->
-<style>
 
-.footer {
-position: fixed;
-  left: 0;
-  bottom: 0;
-  width: 100%;
-  background-color: #343A40;
-  color: white;
-  text-align: center;
-  padding: 10px;
-  overflow: hidden;
-
-}
-</style>
-
-
-<div class="footer ">
+<div class="footer">
   <p class="bg-black text-white">&copy; 2018 <a href="http://susonsapkota.com.np/">Suson Sapkota
   </p>
 </div>
--- header.php
+++ header.php
@@ -13,10 +13,12 @@ if (!isset($title)) {
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
+  <link rel="stylesheet" type="text/css" href="page_style.css">
+  <link rel="icon" href="img/student.png">
 </head>
 <body>
   <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-    <a class="navbar-brand" href="index.html">Student Register</a>
+    <a class="navbar-brand" href="index.html">Student Mgmt.</a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
